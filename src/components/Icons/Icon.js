3c02228882f48566eda0835ae27ae2dec63e5b0c
Titux Metal (DEV) <github@lgdweb.fr>
2022-03-22 09:19:19 +0100

    Adding some Icons components

---
+++ src/components/Icons/Icon.js
@@ -0,0 +1,41 @@
+import tw, { styled } from 'twin.macro'
+
+import AddressIcon from './AddressIcon'
+import ArrowRightIcon from './ArrowRightIcon'
+import DateIcon from './DateIcon'
+
+const Wrapper = styled.div(tw`w-5 h-5 text-white`)
+
+const Icon = ({ children, ...rest }) => <Wrapper {...rest}>{children}</Wrapper>
+
+Icon.Address = ({ ...rest }) => {
+  Icon.Address.displayName = 'AddressIcon'
+
+  return (
+    <Wrapper {...rest}>
+      <AddressIcon />
+    </Wrapper>
+  )
+}
+
+Icon.ArrowRight = ({ ...rest }) => {
+  Icon.ArrowRight.displayName = 'ArrowRightIcon'
+
+  return (
+    <Wrapper {...rest}>
+      <ArrowRightIcon />
+    </Wrapper>
+  )
+}
+
+Icon.Date = ({ ...rest }) => {
+  Icon.Date.displayName = 'DateIcon'
+
+  return (
+    <Wrapper {...rest}>
+      <DateIcon />
+    </Wrapper>
+  )
+}
+
+export default Icon
