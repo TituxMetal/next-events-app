6508a1328f1f7324485264002a906e45ca9706bc
Titux Metal (DEV) <github@lgdweb.fr>
2022-03-23 11:11:21 +0100

    Updating the all events page; adds the EventsSearch component and shows all events

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,19 +1,20 @@
+import { DataList, EventItem, EventsSearch, List } from '~/components'
 import { getAllEvents } from '~/lib'
 
 const AllEventsPage = () => {
   const events = getAllEvents()
 
   return (
-    <main>
-      <h1>All Events</h1>
-      {events.map(event => (
-        <section key={event.id}>
-          <h2>{event.title}</h2>
-          <p>{event.description}</p>
-          <p>{event.location}</p>
-        </section>
-      ))}
-    </main>
+    <>
+      <EventsSearch />
+      <List>
+        {events?.length ? (
+          <DataList itemComponent={EventItem} dataList={events} />
+        ) : (
+          <p>No events yet, please add any</p>
+        )}
+      </List>
+    </>
   )
 }
 
