0e28f2fb63e46e9dbed850db853dfba94f428f21
Titux Metal (DEV) <github@lgdweb.fr>
2022-03-26 06:23:06 +0100

    Updating [...slug].js file; shows events according to the url query

--- src/pages/events/[...slug].js
+++ src/pages/events/[...slug].js
@@ -1,7 +1,48 @@
-const FilteredEventsPage = () => (
-  <main>
-    <h1>Filtered Events Page</h1>
-  </main>
-)
+import { useRouter } from 'next/router'
+import { useEffect, useMemo, useState } from 'react'
+import tw from 'twin.macro'
+
+import { ButtonLink, DataList, EventItem, EventsResults, List } from '~/components'
+import { formatDate, getFilteredEvents } from '~/lib'
+
+const Container = tw.section`flex flex-col mx-auto w-full text-center`
+
+const FilteredEventsPage = () => {
+  const [filter, setFilter] = useState({ events: null, error: '' })
+  const [date, setDate] = useState()
+
+  const { query } = useRouter()
+
+  const filteredData = useMemo(
+    () => query.slug && query.slug.filter(item => !isNaN(item)),
+    [query.slug]
+  )
+
+  useEffect(() => {
+    if (filteredData) {
+      const data = getFilteredEvents(filteredData)
+
+      setDate(formatDate(new Date(filteredData[0], filteredData[1] - 1)))
+      setFilter(state => ({ ...state, ...data }))
+    }
+  }, [filteredData])
+
+  return (
+    <Container>
+      {filter.error ? (
+        <EventsResults>{filter.error}</EventsResults>
+      ) : !filter.events || filter.events.length === 0 ? (
+        <EventsResults>No events found for the choosen filter!</EventsResults>
+      ) : (
+        <>
+          <EventsResults>Events in {date}</EventsResults>
+          <List>
+            <DataList itemComponent={EventItem} dataList={filter.events} />
+          </List>
+        </>
+      )}
+    </Container>
+  )
+}
 
 export default FilteredEventsPage
