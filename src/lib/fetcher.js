f607b9c93a1f368cbf2dc7a9c2b4e203827ec4c2
Titux Metal (DEV) <github@lgdweb.fr>
2022-03-26 06:07:30 +0100

    Updating fetcher.js file in lib; improves getFilteredEvents function by adding params validation and error management

--- src/lib/fetcher.js
+++ src/lib/fetcher.js
@@ -4,14 +4,20 @@ export const getFeaturedEvents = () => DUMMY_EVENTS.filter(event => event.isFeat
 
 export const getAllEvents = () => DUMMY_EVENTS
 
-export const getFilteredEvents = dateFilter => {
-  const { year, month } = dateFilter
+export const getFilteredEvents = (dateFilter = []) => {
+  const [year, month] = dateFilter
+  const isInvalidFilter = isNaN(+year) || isNaN(+month)
+  const message = 'Invalid filter. Please adjust your values!'
+
+  if (isInvalidFilter) {
+    return { events: null, error: message }
+  }
 
   const filteredEvents = DUMMY_EVENTS.filter(
-    ({ date }) => new Date(date).getFullYear() === year && new Date(date).getMonth() === month - 1
+    ({ date }) => new Date(date).getFullYear() === +year && new Date(date).getMonth() === +month - 1
   )
 
-  return filteredEvents
+  return { events: filteredEvents, error: null }
 }
 
 export const getEventById = id => DUMMY_EVENTS.find(event => event.id === id)
