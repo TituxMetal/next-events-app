f4dbedff8cf7c70853257710668b5263423b942c
Titux Metal (DEV) <github@lgdweb.fr>
2022-03-27 12:02:17 +0200

    Adding EventDetails.js file in events; renders details of a single event

--- src/components/events/index.js
+++ src/components/events/index.js
@@ -1,3 +1,4 @@
+export { default as EventDetails } from './EventDetails'
 export { default as EventItem } from './EventItem'
 export { default as EventsResults } from './EventsResults'
 export { default as EventsSearch } from './EventsSearch'
