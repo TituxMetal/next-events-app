d341efb6c4206f17f46328447e32c967877722fd
Titux Metal (DEV) <github@lgdweb.fr>
2022-03-30 04:27:06 +0200

    Updating EventItem.js file in components/events

--- src/components/events/EventItem.js
+++ src/components/events/EventItem.js
@@ -1,47 +1,36 @@
-import Image from 'next/image'
 import Link from 'next/link'
-import tw from 'twin.macro'
+import tw, { styled } from 'twin.macro'
 
-import { ButtonLink, Icon } from '~/components'
+import { AddressInfo, ButtonLink, DateInfo, Icon, Thumbnail } from '~/components'
 import { formatAddress, formatDate } from '~/lib'
+
+const Container = tw.section`mt-2 py-2 flex flex-col gap-2 w-full md:(m-0 w-3/5)`
+const Cta = tw.div`flex flex-row justify-end p-4 mt-3`
+const Spacer = tw.span`ml-2`
+const Title = tw.h2`md:my-6 my-2 text-2xl text-center`
+const Wrapper = tw.li`rounded-xl my-4 overflow-hidden flex flex-col gap-4 border-0 bg-primary md:(flex-row max-w-3xl mx-auto)`
 
 const EventItem = ({ dataId, date, description, image, isFeatured, location, title }) => {
   const humanReadableDate = formatDate(date)
   const formattedAddress = formatAddress(location)
 
   return (
-    <li tw='rounded-xl my-4 overflow-hidden flex flex-col gap-4 border-0 bg-primary md:(flex-row max-w-3xl mx-auto)'>
-      <div tw='md:(h-56 w-2/5 overflow-visible) h-40 w-full overflow-hidden'>
-        <Image
-          src={`/${image}`}
-          alt={title}
-          layout='responsive'
-          objectFit='cover'
-          width={250}
-          height={250}
-          quality='60'
-        />
-      </div>
-      <div tw='md:(w-3/5 text-left) w-full px-4 text-center'>
-        <h2 tw='md:my-6 my-2 text-2xl'>{title}</h2>
-        <div tw='flex gap-2 items-center'>
-          <Icon.Date />
-          <time tw='text-gray-300 font-bold'>{humanReadableDate}</time>
-        </div>
-        <div tw='flex gap-2 items-center'>
-          <Icon.Address />
-          <address tw='text-gray-300 whitespace-pre'>{formattedAddress}</address>
-        </div>
-        <div tw='flex flex-row justify-end p-4 mt-3'>
+    <Wrapper>
+      <Thumbnail image={image} alt={title} />
+      <Container>
+        <Title>{title}</Title>
+        <DateInfo>{humanReadableDate}</DateInfo>
+        <AddressInfo>{formattedAddress}</AddressInfo>
+        <Cta>
           <ButtonLink href={`/events/${dataId}`}>
             Explore event
-            <div tw='ml-2'>
+            <Spacer>
               <Icon.ArrowRight />
-            </div>
+            </Spacer>
           </ButtonLink>
-        </div>
-      </div>
-    </li>
+        </Cta>
+      </Container>
+    </Wrapper>
   )
 }
 
