26292411472952492923f3fae3c50f3459292505
Titux Metal (DEV) <github@lgdweb.fr>
2022-04-07 11:50:27 +0200

    Updating string.js file in lib; refactores formatDate function for better reusability

--- src/lib/strings.js
+++ src/lib/strings.js
@@ -1,8 +1,11 @@
-export const formatDate = date =>
-  new Date(date).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  })
+const dateOptions = {
+  month: 'long',
+  year: 'numeric'
+}
+
+const getOptions = date =>
+  date.split('-').length === 3 ? Object.assign({}, { ...dateOptions, day: 'numeric' }) : dateOptions
+
+export const formatDate = date => new Date(date).toLocaleDateString('en-US', getOptions(date))
 
 export const formatAddress = address => address.replace(', ', '\n')
