f2266ca1ce2edae55badefce59aa057cf465fdfb
Titux Metal (DEV) <github@lgdweb.fr>
2022-04-07 11:49:01 +0200

    Adding index.js file in prisma; ensures that Prisma client instance is only used once, to avoid that too many connections are made to the database, during development

---
+++ prisma/index.js
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: [!isProd && 'warn', 'info', { level: 'query', emit: 'event' }, 'error']
+  })
+
+if (!isProd) global.prisma = prisma
+
+prisma.$on('query', event => console.log(event))
+
+export default prisma
