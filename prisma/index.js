63791fa3243ba05a5b8f329805c79c79d2f557a4
Titux Metal (DEV) <github@lgdweb.fr>
2022-04-09 19:49:23 +0200

    Updating index.js file in prisma; fixes error from database caused by multiple connections

--- prisma/index.js
+++ prisma/index.js
@@ -2,13 +2,16 @@ import { PrismaClient } from '@prisma/client'
 
 const isProd = process.env.NODE_ENV === 'production'
 
+console.log('NODE_ENV', process.env.NODE_ENV)
+
+console.log('global.prisma', !!global.prisma)
 export const prisma =
   global.prisma ||
   new PrismaClient({
     log: [!isProd && 'warn', 'info', { level: 'query', emit: 'event' }, 'error']
   })
 
-if (!isProd) global.prisma = prisma
+if (!global.prisma) global.prisma = prisma
 
 prisma.$on('query', event => console.log(event))
 
