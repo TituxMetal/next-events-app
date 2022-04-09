a2601e0a889483b3be339510e18deed9d0fba5c6
Titux Metal (DEV) <github@lgdweb.fr>
2022-04-09 19:45:01 +0200

    Updating seed.js file in prisma; uses faker-js for generating more fake data

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,47 +1,34 @@
+const { faker } = require('@faker-js/faker')
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
-
-const eventsData = [
-  {
-    title: 'Programming for everyone',
-    description:
-      'Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.',
-    location: 'Somestreet 25, 12345 San Somewhereo',
-    date: '2021-05-12',
-    image: 'images/coding-event.jpg',
-    isFeatured: false
-  },
-  {
-    title: 'Networking for introverts',
-    description:
-      "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
-    location: 'New Wall Street 5, 98765 New Work',
-    date: '2021-05-30',
-    image: 'images/introvert-event.jpg',
-    isFeatured: true
-  },
-  {
-    title: 'Networking for extroverts',
-    description:
-      'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
-    location: 'My Street 12, 10115 Broke City',
-    date: '2022-04-10',
-    image: 'images/extrovert-event.jpg',
-    isFeatured: true
-  }
-]
+const dataAmount = process.env.NODE_ENV === 'production' ? 11 : 100
 
 const main = async () => {
+  const deleted = await prisma.event.deleteMany({})
+  console.log(`Cleanning ${deleted.count} database data...`)
   console.log('Start seeding...')
 
-  for (const e of eventsData) {
-    const event = await prisma.event.create({ data: e })
+  const data = Array.from(Array(dataAmount).keys(), async () => {
+    const event = await prisma.event.create({
+      data: {
+        title: faker.fake(
+          '{{hacker.abbreviation}} {{hacker.ingverb}} {{hacker.verb}} {{hacker.noun}}'
+        ),
+        description: faker.lorem.sentences(),
+        location: faker.fake(
+          '{{address.streetAddress(true)}}, {{address.zipCode("#####")}} {{address.city}}'
+        ),
+        date: faker.date.soon(365),
+        image: `https://picsum.photos/seed/${faker.word.verb()}/640/480`,
+        isFeatured: faker.datatype.boolean()
+      }
+    })
 
     console.log(`Event created with id: ${event.id}`)
-  }
+  })
 
-  console.log('Seeding finished.')
+  console.log(`Seeding finished with ${data.length} events created.`)
 }
 
 main()
