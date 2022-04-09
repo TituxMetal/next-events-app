e7114d0f051bbfd8f02b24edbe0a186d307b60dd
Titux Metal (DEV) <github@lgdweb.fr>
2022-04-09 20:11:34 +0200

    Removes console.log

--- prisma/index.js
+++ prisma/index.js
@@ -2,9 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 const isProd = process.env.NODE_ENV === 'production'
 
-console.log('NODE_ENV', process.env.NODE_ENV)
-
-console.log('global.prisma', !!global.prisma)
 export const prisma =
   global.prisma ||
   new PrismaClient({
